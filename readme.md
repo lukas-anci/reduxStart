07804014e3735b91ba6ae7c9250dc58397bb5b85
Lukas <lukas.ancikevicius@gmail.com>
2021-08-24 09:34:18 +0300

    created action

--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@
 
 1. npm i redux react-redux
 2. susikurt redux pafr faila
+3. aprasom actions ir state
+4. provide the store
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,24 @@
 import { createStore } from 'redux';
 
+const initialCounterState = { counter: 0 };
+
 // reducer fn
-const counterReducer = () => {};
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case 'UP':
+      return {
+        counter: state.counter + 1,
+      };
+    case 'DOWN':
+      return {
+        counter: state.counter - 1,
+      };
+
+    default:
+      return state;
+  }
+};
 // store
 const store = createStore(counterReducer);
+
+export default store;
