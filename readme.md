40e4f323e48e004c20d2ea3d0e13e201f30adf3d
Lukas <lukas.ancikevicius@gmail.com>
2021-08-24 10:01:17 +0300

    dispatch up and down actions

--- readme.md
+++ readme.md
@@ -3,4 +3,7 @@
 1. npm i redux react-redux
 2. susikurt redux pafr faila
 3. aprasom actions ir state
-4. provide the store
+4. provide the store in index.js for <App />
+5. use store data useSelector hook
+6. useDipatch hook to make action
+7.
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter); // automatiskai sukuria subscribe
+
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: 'UP' });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: 'DOWN' });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Up</button>
+        <button onClick={decrementHandler}>Down</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
